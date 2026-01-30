6d3e7169b23085120f20aa925c0cadf045f1e167
nanontelo87 <anteloma87@gmail.com>
2026-01-30 18:49:32 -0300

    proyecto react parte 2 terminado

--- NavegaLasRutas-Antelo/src/components/NavBar/NavBar.jsx
+++ NavegaLasRutas-Antelo/src/components/NavBar/NavBar.jsx
@@ -1,4 +1,74 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import CartWidget from "../CartWidget";
+import Logo from "../../Fotos/Logo.png";
+import "./NavBar.css";
+
+const NavBar = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+  }, []);
+
+  return (
+    <header className="navbar">
+      <div className="navbar-container">
+
+        <div className="navbar-start">
+          <Link to="/" className="navbar-brand">
+            <img src={Logo} alt="Logo" />
+          </Link>
+        </div>
+
+        <div className="navbar-center">
+          <ul className="menu">
+            <li>
+              <details>
+                <summary>Categorías</summary>
+
+                <ul className="dropdown-menu">
+                  {categories.map((cat) => (
+                    <li key={cat.slug}>
+                      <Link to={`/category/${cat.slug}`}>
+                        {cat.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+
+        <div className="navbar-end">
+          <CartWidget />
+        </div>
+
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { Link } from "react-router-dom";
 import { useState } from "react";
 import CartWidget from "../CartWidget";
 import Logo from "../../Fotos/Logo.png";
@@ -73,4 +143,4 @@ const NavBar = () => {
   );
 };
 
-export default NavBar;
+export default NavBar;*/
